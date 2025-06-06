f33c5be7c6e8f784ad93d97a1063dd3258204bd4
mia-vrdoljak <miavrdoljak2@gmail.com>
2025-06-06 15:20:44 +0200

    Novo

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Novo");
+        }
+    }
+}
